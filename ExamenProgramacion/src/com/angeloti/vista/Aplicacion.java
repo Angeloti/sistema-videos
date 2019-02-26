6dae2438d28c6c649b6309a5c06a554c359da375
User <User@DESKTOP-UL6CGAP>
2019-02-26 07:10:58 -0500

    trabajo colaborativo1.2 David Vega

---
+++ ExamenProgramacion/src/com/angeloti/vista/Aplicacion.java
@@ -0,0 +1,15 @@
+package com.angeloti.vista;
+
+
+import com.angeloti.modelo.VentanaPuntos;
+
+/**
+ *
+ * @author Casa
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        new VentanaPuntos();
+    }
+    
+}
